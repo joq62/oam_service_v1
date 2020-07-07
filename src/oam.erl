56e362d11920a6eaa3b82fddde97adccfce48198
joq62 <joakim.leche@gmail.com>
2020-07-07 23:14:24 +0200

    corr

--- src/oam.erl
+++ src/oam.erl
@@ -26,5 +26,15 @@
 %% ====================================================================
 log_get(Type)->
     LogServices=dns_service:get("log_service"),
-    [{Node,rpc:call(Node,log_service,get,[Type])}||{"log_service",Node}<-LogServices].
+    LogInfo=[{Node,rpc:call(Node,log_service,get,[Type])}||{"log_service",Node}<-LogServices],
+    R=[{calendar:datetime_to_gregorian_seconds({{Y,M,D},{H,Min,S}}),{Type1,[Node,Module,File,Line,{Y,M,D},{H,Min,S},Msg]}}||{Type1,[Node,Module,File,Line,{Y,M,D},{H,Min,S},Msg]}<-LogInfo],
+    qsort(R).
+    
+
+qsort([])->[];
+qsort([{Pivot,Info}|T])->
+    qsort([{Type,[Node,Module,File,Line,{Y,M,D},{H,Min,S},Msg]}||{DateSeconds,{Type,[Node,Module,File,Line,{Y,M,D},{H,Min,S},Msg]}}<-T,
+								     DateSeconds<Pivot]) ++ [Info] ++
+    qsort([{Type,[Node,Module,File,Line,{Y,M,D},{H,Min,S},Msg]}||{DateSeconds,{Type,[Node,Module,File,Line,{Y,M,D},{H,Min,S},Msg]}}<-T,
+									 DateSeconds>=Pivot]).
     
